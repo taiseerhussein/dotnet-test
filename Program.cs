080434d98459f8140eef8bd373f679ff32f88790
Taiseer Hussein <thussein@redhat.com>
2024-05-16 09:28:02 -0500

    add the triggers

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var counter = 0;
 var max = 250;
 while (counter < max)
 {
-    Console.WriteLine($"Counter: {++counter}");
+    Console.WriteLine($"Counter test: {++counter}");
     await Task.Delay(TimeSpan.FromMilliseconds(1_000));
     ++counter;
 }
